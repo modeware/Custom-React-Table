80ec3ef0226b0093b589a63ad554157d896a2f15
Safiq Lodi <safiqlodi2@gmail.com>
2022-11-23 10:42:00 +0530

    Scaffolded React app with create-react-app

---
+++ src/components/Table/table.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+
+const Table: FC<any> = ({headers, tableRows}) => {
+
+    return <table>
+        <tr>
+            <td>Hey</td>
+            <td>Hey</td>
+            <td>Hey</td>
+        </tr>
+        <tr>
+            <td>Hey</td>
+            <td>Hey</td>
+            <td>Hey</td>
+        </tr>
+        <tr>
+            <td>Hey</td>
+            <td>Hey</td>
+            <td>Hey</td>
+        </tr>
+    </table>
+
+}
+
+
+export default Table;
---
+++ src/components/Table/tableInterface.tsx
@@ -0,0 +1,4 @@
+export interface Table{
+    headers: Array<any>
+    tableRows: Array<any>
+}
